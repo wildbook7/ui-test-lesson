3353d2a748ea087e37b2ed71a078fef3bae131b1
wildbook7 <wildbook7@gmail.com>
2024-11-27 12:51:18 +0900

    Storybook に Button コンポーネントを追加

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,21 @@
 interface ButtonProps {
   label: string;
+  primary?: boolean;
   onClick: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
-  return <button onClick={onClick}>{label}</button>;
+const Button: React.FC<ButtonProps> = ({ label, primary, onClick }) => {
+  const buttonStyle = primary
+    ? {
+        backgroundColor: "blue",
+        color: "white",
+      }
+    : { backgroundColor: "white", color: "blue" };
+  return (
+    <button style={buttonStyle} onClick={onClick}>
+      {label}
+    </button>
+  );
 };
 
 export default Button;
